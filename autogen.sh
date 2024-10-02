c742035c8fba0c6999a1b218dceaec59419a0187
bpdegnan <degnan.68k@gmail.com>
2024-10-02 10:46:02 -0400

    make a script to do the dirty work

---
+++ autogen.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+ACLOCAL="aclocal"
+AUTOHEADER="autoheader"
+AUTOMAKE="automake"
+AUTOCONF="autoconf"
+
+echo "running aclocal"
+mkdir scripts
+${ACLOCAL} ${ACLOCAL_ARGS} -I scripts
+
+echo "running autoheader"
+${AUTOHEADER}
+
+echo "running automake"
+${AUTOMAKE} --add-missing -c --foreign
+
+echo "running autoconf"
+${AUTOCONF} -f -i
+
+if [ "${1}" = '-c' ]; then
+    echo "running configure"
+    ./configure
+fi
